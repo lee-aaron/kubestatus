deaf833ca4fd36f55a885db76fdbd7b702fd2ee7
lee-aaron <lee.aaron.68@gmail.com>
2021-05-24 12:33:12 -0700

    Fixed ingress

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kubestatus
-Kubernetes Work
+Kubernetes Work on Windows WSL2
+
+## Getting Started
+
+Use `minikube` but do not enable ingress addon. Use a cloud deployment yml file instead.
+
+Then run `kubectl apply -f k8s` to get your services up. Run `minikube tunnel` to get map localhost on windows to the WSL2 subnet.
--- api/main.go
+++ api/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+	})
+
+	r.GET("/api/ping", func(c *gin.Context) {
+		fmt.Println("ping")
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
-	r.Run()
+	r.Run(":8080")
 }
